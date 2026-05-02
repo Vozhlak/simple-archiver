ae9cf6ca2ccb43b7e91b1cc15356aa3f6cd0ebb3
ilya.besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2026-05-02 20:38:12 +0300

    feat: add method countRepeating

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	DefaultBufferSize = 1024 * 8 // 8KB буфер
@@ -26,6 +28,31 @@ func (sa *SimpleArchiver) compressEmpty(data []byte) []byte {
 	}
 
 	return data
+}
+
+func (sa *SimpleArchiver) countRepeating(data []byte) []byte {
+	if len(data) == 0 {
+		return []byte{}
+	}
+
+	result := make([]byte, 0, len(data)*2)
+
+	current := data[0]
+	count := 1
+
+	for i := 1; i < len(data); i++ {
+		if data[i] == current {
+			count++
+		} else {
+			result = append(result, byte(count)+'0', current)
+			current = data[i]
+			count = 1
+		}
+	}
+
+	result = append(result, byte(count)+'0', current)
+
+	return result
 }
 
 func main() {
