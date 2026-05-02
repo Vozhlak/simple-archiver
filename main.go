dd5e56a0108a5c8c4fd5a09443af6dda226c0c4d
ilya.besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2026-05-02 17:53:28 +0300

    feat: add SimpleArchiver with NewArchiver constructor and .gitignore
    - Add SimpleArchiver struct with InputPath, OutputPath, Buffer
    - Add NewArchiver constructor with default buffer size
    - Add .gitignore for Go project

--- main.go
+++ main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+const (
+	DefaultBufferSize = 1024 * 8 // 8KB буфер
+)
+
+type SimpleArchiver struct {
+	inputPath  string
+	outputPath string
+	buffer     []byte
+}
+
+func NewArchiver(inputPath string) *SimpleArchiver {
+	return &SimpleArchiver{
+		inputPath:  inputPath,
+		outputPath: "",
+		buffer:     make([]byte, DefaultBufferSize),
+	}
+}
+
 func main() {
 	fmt.Println("Happy coding!!!")
 }
