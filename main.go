961f919f92a34004637a61ac0382ef4962fe0ded
ilya.besprozvannykh <ilya.besprozvannykh@redcollar.ru>
2026-05-03 20:09:26 +0300

    feat: add method scanLookaheadGroups

--- main.go
+++ main.go
@@ -67,6 +67,48 @@ func (sa *SimpleArchiver) createControlByte(count int, isCompressed bool) byte {
 	return byte(count)
 }
 
+func (sa *SimpleArchiver) scanLookaheadGroups(data []byte) []byte {
+	result := []byte{}
+	i := 0
+
+	for i < len(data) {
+		runLen := 1
+		for i+runLen < len(data) && data[i+runLen] == data[i] {
+			runLen++
+		}
+
+		if runLen >= 4 {
+			compressed := sa.countRepeating(data[i : i+runLen])
+			ctrl := sa.createControlByte(runLen, true)
+			result = append(result, ctrl)
+			result = append(result, compressed...)
+			i += runLen
+		} else {
+			groupStart := i
+
+			for i < len(data) {
+				nextRun := 1
+				for i+nextRun < len(data) && data[i+nextRun] == data[i] {
+					nextRun++
+				}
+
+				if nextRun >= 4 {
+					break
+				}
+
+				i += nextRun
+			}
+
+			groupLen := i - groupStart
+			ctrl := sa.createControlByte(groupLen, false)
+			result = append(result, ctrl)
+			result = append(result, data[groupStart:i]...)
+		}
+	}
+
+	return result
+}
+
 func main() {
 	fmt.Println("Happy coding!!!")
 }
